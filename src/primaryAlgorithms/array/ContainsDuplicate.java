7e3c6a08775c93260b28b9d40b501ac00dc9557e
showanswer <1310151064@qq.com>
2022-12-14 23:49:08 +0800

    算法： 2022-12-14数组学习

---
+++ src/primaryAlgorithms/array/ContainsDuplicate.java
@@ -0,0 +1,77 @@
+package primaryAlgorithms.array;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author answer
+ * @Description: <p>存在重复元素</p>
+ * @date 2022/12/14 22:00
+ */
+public class ContainsDuplicate {
+
+    /**
+     * 给你一个整数数组 nums 。如果任一值在数组中出现 至少两次 ，返回 true ；如果数组中每个元素互不相同，返回 false 。
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        ContainsDuplicate test = new ContainsDuplicate();
+        int[] nums = new int[]{1, 2, 3, 4};
+        boolean flag = test.containsDuplicate2(nums);
+        System.out.println(flag);
+    }
+
+    /**
+     * nums = [1,2,3,1] => true
+     * nums = [1,2,3,4] => false
+     * nums = [1,1,1,3,3,4,3,2,4,2] => true
+     *
+     * @param nums
+     * @return 思路： 1. 只要有重复的数值就返回true，否则返回false， 用set可以直接解决
+     */
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!set.add(nums[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 思路2： 暴力破解法： 直接双循环比较是否有相同的值
+     *
+     * @param nums
+     * @return
+     */
+    public boolean containsDuplicate1(int[] nums) {
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 思路3: 可以先排序， 排序后在前后比较
+     *
+     * @param nums
+     * @return
+     */
+    public boolean containsDuplicate2(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] == nums[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/primaryAlgorithms/array/SingleNumber.java
@@ -0,0 +1,64 @@
+package primaryAlgorithms.array;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author answer
+ * @Description: <p>只出现一次的数字</p>
+ * @date 2022/12/14 22:12
+ */
+public class SingleNumber {
+
+    /**
+     * 给你一个 非空 整数数组 nums ，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+     * 你必须设计并实现线性时间复杂度的算法来解决此问题，且该算法只使用常量额外空间。
+     * @param args
+     */
+    public static void main(String[] args) {
+        SingleNumber test = new SingleNumber();
+        int [] nums= new int[]{4,1,2,1,2};
+        int num = test.singleNumber1(nums);
+        System.out.println(num);
+    }
+
+    /**
+     * nums = [2,2,1]=>1, nums = [4,1,2,1,2]=>4, nums = [1]=>1
+     * @param nums
+     * @return
+     * 思路1： 判断某个字符只出现一次， 可以haspMap key为字符， value为出现的次数
+     */
+    public int singleNumber(int[] nums) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            Integer value = entry.getValue();
+            if (value.equals(1)) {
+                return entry.getKey();
+            }
+        }
+        return 0;
+    }
+
+    /**
+     * 思路2： 利用set, 把数据存到set，如果之前一家存在就remove删除掉。 题中声明了其余元素只出现两次
+     *
+     * @param nums
+     * @return
+     */
+    public int singleNumber1(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)) {
+                set.remove(num);
+            }
+        }
+        return (Integer) set.toArray()[0];
+    }
+
+}
