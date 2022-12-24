72a4e18ec02c731855595d5be5346d7d5cb8ceef
showanswer <1310151064@qq.com>
2022-12-24 20:07:03 +0800

    算法： 2022-12-24 面试算法学习

---
+++ src/primaryAlgorithms/string/MaxFrequencyStr.java
@@ -0,0 +1,41 @@
+package primaryAlgorithms.string;
+
+import java.util.HashMap;
+
+/**
+ * @author answer
+ * @Description: <p>给定一个字符串，返回该字符中出现频率最多的字符的出现次数</p>
+ * @date 2022/12/24 16:18
+ */
+public class MaxFrequencyStr {
+
+    /**
+     * 给定一个字符串，返回该字符中出现频率最多的字符的出现次数
+     * 注1： 大小写不区分
+     * 注2： 字符串中 只包含字母或数字
+     * @param args
+     */
+    public static void main(String[] args) {
+        MaxFrequencyStr test = new MaxFrequencyStr();
+        int num = test.maxFrequencyStr("aabbcde");
+        System.out.println(num);
+    }
+
+    /**
+     * "abcde" =>1; "aabbcde" =>2; "aAl1" =>1
+     * @param str
+     * @return
+     * 思路： 利用hashMap 进行存储， key为字符， value为出现的次数
+     */
+    public int maxFrequencyStr(String str) {
+        HashMap<Character, Integer > map = new HashMap<>();
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char c = Character.toLowerCase(chars[i]);
+            map.put(c, map.getOrDefault(c, 0)+1);
+        }
+        Integer num = map.values().stream().max(Integer::compareTo).get();
+        return num;
+    }
+
+}
---
+++ src/primaryAlgorithms/string/TwoNumAdd.java
@@ -0,0 +1,45 @@
+package primaryAlgorithms.string;
+
+import java.util.Arrays;
+
+/**
+ * @author answer
+ * @Description: <p>给定任意两个数： 求数中间的数累加的总值</p>
+ * @date 2022/12/23 21:55
+ */
+public class TwoNumAdd {
+
+    public static void main(String[] args) {
+        TwoNumAdd test = new TwoNumAdd();
+        Integer res = test.twoNumAdd(-3, -6);
+        System.out.println(res);
+    }
+
+    /**
+     * [1, 4]=>(1+2+3+4)= 10; [-3, 2]=> (-3-2-1+0+1+2) =-3;  [-3, -6]=> (-3 -4 -5 -6)=-18
+     * @param num1
+     * @param num2
+     * @return
+     * 思路： 此题以下有三种情况：
+     *       [1,4]=>(4-1)=3中间隔离 3个值；(1+2+3+4)累加差四个值。
+     *       [-3,2]=>(2-（-3）)=5中间隔离 5个值； (-3-2-1+0+1+2)累加差五个值
+     *       [-3,-6]=>(-3-（-6）)=3中间隔离 3个值；(-3 -4 -5 -6)累加差四个值
+     *       先判断大小， （大值-小值）刚好都是相减的值+1。在小值累加 可得到值
+     */
+    public Integer twoNumAdd(int num1, int num2) {
+        int[] array = new int[]{num1, num2};
+        // 排序 由小到大
+        Arrays.sort(array);
+        num1 = array[0];
+        num2 = array[1];
+
+        int result =num1;
+        int len = num2 - num1;
+        for (int i = 0; i < len; i++) {
+            int temp =  ++num1;
+            result = result + temp;
+        }
+        return result;
+    }
+
+}
