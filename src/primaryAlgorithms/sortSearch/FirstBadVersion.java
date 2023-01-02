5efbe2948e478e7e0c63cb2b4b3a4126ea702324
showanswer <1310151064@qq.com>
2023-01-02 21:48:51 +0800

    算法： 2023-1-2 排序与搜索

---
+++ src/primaryAlgorithms/sortSearch/FirstBadVersion.java
@@ -0,0 +1,46 @@
+package primaryAlgorithms.sortSearch;
+
+/**
+ * @author answer
+ * @Description: <p>第一个错误的版本</p>
+ * @date 2023/1/2 21:38
+ */
+public class FirstBadVersion {
+
+    /**
+     * 你是产品经理，目前正在带领一个团队开发新的产品。不幸的是，你的产品的最新版本没有通过质量检测。
+     * 由于每个版本都是基于之前的版本开发的，所以错误的版本之后的所有版本都是错的。
+     * 假设你有 n 个版本 [1, 2, ..., n]，你想找出导致之后所有版本出错的第一个错误的版本。
+     * 你可以通过调用 bool isBadVersion(version) 接口来判断版本号 version 是否在单元测试中出错。
+     * 实现一个函数来查找第一个错误的版本。你应该尽量减少对调用 API 的次数。
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        FirstBadVersion test = new FirstBadVersion();
+        int version = test.firstBadVersion(5);
+        System.out.println(version);
+    }
+
+    /**
+     * n = 5, bad = 4 => 4; n = 1, bad = 1=>1
+     * @param n
+     * @return
+     * 思路： 二分查找，先从中间版本开始调用api测试
+     */
+    public int firstBadVersion(int n) {
+        int start = 1, end = n;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (!isBadVersion(mid)) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return start;
+    }
+
+
+    boolean isBadVersion(int version){return true;};
+}
---
+++ src/primaryAlgorithms/sortSearch/Merge.java
@@ -0,0 +1,68 @@
+package primaryAlgorithms.sortSearch;
+
+/**
+ * @author answer
+ * @Description: <p>合并两个有序数组</p>
+ * @date 2023/1/2 21:22
+ */
+public class Merge {
+
+
+    /**
+     * 给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
+     * 请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
+     * 注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。
+     * 为了应对这种情况，nums1 的初始长度为 m + n，其中前 m 个元素表示应合并的元素，后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。
+     * @param args
+     */
+    public static void main(String[] args) {
+        Merge merge = new Merge();
+        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums2 = {2,5,6};
+        merge.merge(nums1, 3, nums2, 3);
+    }
+
+    /**
+     * nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3 =>[1,2,2,3,5,6]
+     * nums1 = [1], m = 1, nums2 = [], n = 0 => [1]
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     */
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] temp = new int[m + n];
+        int index = 0;
+        int i = 0;
+        int j = 0;
+
+        while (i < m && j < n) {
+            if (nums1[i] <= nums2[j]) {
+                temp[index++] = nums1[i++];
+            } else {
+                temp[index++] = nums2[j++];
+            }
+        }
+
+        for (; i < m; ) {
+            temp[index++] = nums1[i++];
+        }
+        for (; j < n; ) {
+            temp[index++] = nums2[j++];
+        }
+        //再把数组temp中的值赋给nums1
+        for (int k = 0; k < m + n; k++) {
+            nums1[k] = temp[k];
+        }
+    }
+
+    public void merge1(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int end = m + n - 1;
+        while (j >= 0) {
+            nums1[end--] = (i >= 0 && nums1[i] > nums2[j]) ? nums1[i--] : nums2[j--];
+        }
+    }
+
+}
