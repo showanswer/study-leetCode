638c63b7f91f6bf29a96bcea24886d9bd1339b0a
showanswer <1310151064@qq.com>
2023-03-01 23:03:03 +0800

    算法： 2023-03-01 双指针

---
+++ src/primaryAlgorithms/DoublePointer/FindMaxConsecutiveOnes.java
@@ -0,0 +1,40 @@
+package primaryAlgorithms.DoublePointer;
+
+/**
+ * @author answer
+ * @Description: <p>最大连续1的个数</p>
+ * @date 2023/3/1 22:06
+ */
+public class FindMaxConsecutiveOnes {
+
+    /**
+     * 给定一个二进制数组 nums ， 计算其中最大连续 1 的个数。
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        FindMaxConsecutiveOnes test = new FindMaxConsecutiveOnes();
+        int[] nums = {1,0,1,1,0};
+        System.out.println(test.findMaxConsecutiveOnes(nums));
+    }
+
+    /**
+     * nums = [1,1,0,1,1,1] => 3
+     *
+     * @param nums
+     * @return
+     */
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max=0,temp=0;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i]==1){
+                temp++;
+            }else{
+                temp=0;
+            }
+            max = Math.max(max,temp);
+        }
+        return  max;
+    }
+
+}
---
+++ src/primaryAlgorithms/DoublePointer/RemoveElement.java
@@ -0,0 +1,50 @@
+package primaryAlgorithms.DoublePointer;
+
+/**
+ * @author answer
+ * @Description: <p>双指针-移除元素</p>
+ * @date 2023/3/1 21:20
+ */
+public class RemoveElement {
+
+    /**
+     * 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+     * @param args
+     */
+    public static void main(String[] args) {
+        RemoveElement test = new RemoveElement();
+        int[] nums ={0,1,2,2,3,0,4,2};
+        System.out.println(test.removeElement(nums, 2));
+    }
+
+    /**
+     * nums = [0,1,2,2,3,0,4,2], val = 2 => 5, nums = [0,1,4,0,3]
+     * @param nums
+     * @param val
+     * @return
+     */
+    public int removeElement(int[] nums, int val) {
+        int left = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] == val){
+                continue;
+            }
+            nums[left++] = nums[i];
+        }
+        return left;
+    }
+
+
+    public int removeElement1(int[] nums, int val) {
+        int j=0;
+        for (int i = 0; i < nums.length; i++) {
+           if (nums[i] == val) {
+               j++;
+           } else {
+               nums[i-j] = nums[i];
+           }
+        }
+        return nums.length-j;
+    }
+
+}
